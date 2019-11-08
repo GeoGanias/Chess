524329e4a30fea98cb725e962b6fe972d798652a
GeoGanias <geoganias@gmail.com>
2019-11-08 18:12:44 +0200

    Added the implementation

---
+++ game.cpp
@@ -0,0 +1,15 @@
+#include "ChessInterface.hpp"
+
+int main(int argc, const char *argv[]){
+    ChessBoard* board = new ChessBoard();
+    int select_x=4,select_y=4;
+
+    board->Display();
+
+
+    while(board->ArrowKeyInput(&select_x,&select_y)){}
+
+    delete board;
+    
+    return 0;
+}
